1eab3c4dbeb77a28f95afec93f2af6e3d2516db5
Vinoth Ram S S <rhinostanite@gmail.com>
2017-03-19 11:42:32 +0530

    Initial commit

---
+++ app/src/main/java/io/github/Vinoth/bookexampledemo/BookListAdapter.java
@@ -0,0 +1,43 @@
+package io.github.Vinoth.bookexampledemo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.google.api.services.books.model.Volume;
+
+import java.util.List;
+
+/**
+ * Created by cloubao on 1/20/16.
+ */
+public class BookListAdapter extends RecyclerView.Adapter<BookViewHolder> {
+
+    private final int spanCount;
+    private List<Volume> volumes;
+
+    public BookListAdapter(List<Volume> volumes, int spanCount) {
+        this.volumes = volumes;
+        this.spanCount = spanCount;
+    }
+
+    @Override
+    public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new BookViewHolder(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(BookViewHolder holder, int position) {
+        holder.setSpanCount(spanCount);
+        holder.setVolume(volumes.get(position));
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return volumes.get(position).getId().hashCode();
+    }
+
+    @Override
+    public int getItemCount() {
+        return volumes.size();
+    }
+}
